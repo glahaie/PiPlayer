25d1eb30744f95f2ceb70afe2b8167ceecee077f
Guillaume Lahaie <glahaie@gmail.com>
2017-08-29 20:39:49 -0400

    Simple api for now

--- flask_player/flask_player.py
+++ flask_player/flask_player.py
@@ -1,15 +1,45 @@
+#! /usr/bin/python
+
+# Small app to play radio streams in VLC
+
 from flask import Flask, request
 from flask_restful import Resource, Api
+import vlc
+
+# TODO : move in a class of its own
+vlc_instance = vlc.Instance()
+vlc_player = vlc_instance.media_player_new(
+    "http://2QMTL0.akacast.akamaistream.net:80/7/953/177387/v1/rc.akacast.akamaistream.net/2QMTL0")
 
 app = Flask(__name__)
 api = Api(app)
 
-class FlaskPlayer(Resource):
-	def put(self):
-		radio_station = request.form['data']
-		return {radio_station : radio_station}
 
-api.add_resource(FlaskPlayer, '/play')		
+class FlaskPlayer(Resource):
+
+    def put(self, todo_id):
+
+    	print("todo_id = " + todo_id)
+    	if todo_id == "play":
+    		self.__play()
+    	elif todo_id == "stop":
+    		self.__stop()
+
+    def __play(self):
+    	"""Starts Radio-canada
+    	"""
+
+    	print("Entering play()")
+    	vlc_player.play()
+    	return {radio_station: "radio-canada première"}
+
+    def __stop(self):
+    	"""Stops Radio-canada
+    	"""
+    	vlc_player.stop()
+    	return {}
+
+api.add_resource(FlaskPlayer, '/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
